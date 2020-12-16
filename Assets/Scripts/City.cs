a7cacd61aa3e2550698c0f0e25f8e5983135a66f
ProMix <misha.zavertkin@mail.ru>
2020-12-16 23:28:18 +0600

    Building interfaces blueprints completed

---
+++ Assets/Scripts/City.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class City
+    {
+        public List<Building> Buildings { get; } = new List<Building>();
+        private Building[,] Grid;
+    }
+}
