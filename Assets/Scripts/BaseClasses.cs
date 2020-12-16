9bfada5f906c9c1590ab8d2b2830d551bb4ef0fb
ProMix <misha.zavertkin@mail.ru>
2020-12-16 15:31:05 +0600

    Methods to implement added

---
+++ Assets/Scripts/BaseClasses.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Building
+{
+    private GameObject model;
+    private City cityParent;
+
+    public Building(string path, City city)
+    {
+        cityParent = city;
+        //TODO добавление 3D модели
+    }
+    public void SetCoordinates(int x, int y, int z)
+    {
+        model.transform.position = new Vector3(x, y, z);
+    }
+    public void Show()
+    {
+        //TODO метод, который выводит модель на экран и делает её статичной (запрет на перемещение)
+    }
+}
+
+public class City
+{
+    public List<Building> Buildings { get; } = new List<Building>();
+}
