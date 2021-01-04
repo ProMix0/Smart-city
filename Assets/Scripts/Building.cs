cfb667829cdc74a585c34a142a2194d617381220
k0te1k4 <egor.gmag@yandex.ru>
2021-01-04 17:56:50 +0700

    UI

---
+++ Assets/Scripts/Building.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game
+{
+    public class Building:MonoBehaviour
+    {
+        private GameObject model;
+        private City cityParent;
+
+        public Building(string path, City city)
+        {
+            cityParent = city;
+            model = Instantiate(Resources.Load(path, typeof(GameObject))) as GameObject;
+        }
+        public void SetCoordinates(int x, int y, int z)
+        {
+            model.transform.position = new Vector3(x, y, z);
+        }
+    }
+}
---
+++ Assets/Scripts/Resources.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Resources : MonoBehaviour
+{
+    public Text text;
+
+    public int r1 = 1337;
+    
+    public void move()
+    {
+        GameObject.FindWithTag("MainCamera").transform.position += new Vector3(1, 1, 1);
+    }
+    public void test()
+    {
+        text.text += "+";
+    }
+
+    void Update()
+    {
+    }
+}
+
