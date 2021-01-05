bc8b5e761edc03b77eecb32ac944a5af4b0bb1f5
k0te1k4 <egor.gmag@yandex.ru>
2021-01-06 02:42:17 +0700

    UI upgraded

---
+++ Assets/Scripts/ViewSW.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewSW : MonoBehaviour
+{
+    private Camera GameCam;
+    private Camera MenuCam;
+    void Start()
+    {
+        GameCam = GetComponent<Camera>();
+        GameCam = Camera.main;
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.T))
+        {
+            GameCam.enabled = !GameCam.enabled;
+            MenuCam.enabled = !MenuCam.enabled;
+        }
+    }
+}
