cd20fb4d6456443ba35222763d27962a3259bcf1
k0te1k4 <egor.gmag@yandex.ru>
2021-01-06 17:19:17 +0700

    Menu added, ui upgraded

--- Assets/Scripts/Resources.cs
+++ Assets/Scripts/Resources.cs
@@ -8,5 +8,18 @@ public class Resources : MonoBehaviour
     public Text text;
 
     public int r1 = 1337;
+    
+    public void move()
+    {
+        GameObject.FindWithTag("MainCamera").transform.position += new Vector3(1, 1, 1);
+    }
+    public void test()
+    {
+        text.text += "+";
+    }
+
+    void Update()
+    {
+    }
 }
 
