6c603252e44d26edbf1fe44f5f5c8145f7cfffa4
ProMix <misha.zavertkin@mail.ru>
2021-01-07 21:15:06 +0700

    Grid painting completed

--- Assets/Scripts/City.cs
+++ Assets/Scripts/City.cs
@@ -7,7 +7,9 @@ namespace Game
 {
     public class City : MonoBehaviour
     {
-        public Building[,] Grid { get; private set; } = new Building[100, 100];
+        public const int GridSideSize = 100, CellSizeAsCoordinates = 50;
+
+        public Building[,] Grid { get; private set; } = new Building[GridSideSize, GridSideSize];
         public int Radius { get;private set; }
         public List<Building> Buildings { get; } = new List<Building>();
         public List<Home> Homes { get; } = new List<Home>();
@@ -52,6 +54,34 @@ namespace Game
         public double Resources { get; set; }
         public int Points { get; private set; }
 
+        public void Start()
+        {
+            int yCoordinate = 0, xCoordinate = GridSideSize * CellSizeAsCoordinates / 2, zCoordinate = xCoordinate;
+            List<Vector3> lines = new List<Vector3> { new Vector3(xCoordinate, yCoordinate, -zCoordinate),
+                new Vector3(xCoordinate, yCoordinate, zCoordinate) };
+            while (xCoordinate > -GridSideSize * CellSizeAsCoordinates / 2)
+            {
+                xCoordinate -= CellSizeAsCoordinates;
+                lines.Add(new Vector3(xCoordinate, yCoordinate, zCoordinate));
+                zCoordinate = -zCoordinate;
+                lines.Add(new Vector3(xCoordinate, yCoordinate, zCoordinate));
+            }
+            xCoordinate = -xCoordinate;
+            lines.Add(new Vector3(xCoordinate, yCoordinate, zCoordinate));
+            while (zCoordinate > -GridSideSize * CellSizeAsCoordinates / 2)
+            {
+                zCoordinate -= CellSizeAsCoordinates;
+                lines.Add(new Vector3(xCoordinate, yCoordinate, zCoordinate));
+                xCoordinate = -xCoordinate;
+                lines.Add(new Vector3(xCoordinate, yCoordinate, zCoordinate));
+            }
+            GameObject visualGrid = new GameObject();
+            LineRenderer lineRenderer= visualGrid.AddComponent<LineRenderer>();
+            lineRenderer.positionCount = lines.Count;
+            lineRenderer.SetPositions(lines.ToArray());
+
+        }
+
         public void OnBuildBuilding(Building building)
         {
             foreach (var item in Homes)
@@ -67,7 +97,8 @@ namespace Game
             Plane plane = new Plane(Vector3.up, Vector3.zero);
             plane.Raycast(ray, out float distance);
             Vector3 point = ray.GetPoint(distance);
-            return new Tuple<int, int>((int)(point.x/25+50), (int)(point.y / 25 + 50));
+            return new Tuple<int, int>((int)(point.x / CellSizeAsCoordinates + GridSideSize / 2), 
+                (int)(point.y / CellSizeAsCoordinates + GridSideSize / 2));
         }
 
         public enum GameStage
